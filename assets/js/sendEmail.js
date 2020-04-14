eb1658d69a397a72e6ee54eeb674956d6ad77229
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-14 21:40:04 +0200

    Finally got it to send an email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,10 +1,11 @@
 function sendMail(contactForm) {
+    console.log("check");
     emailjs.send("gmail", "binck_explores", {
         "from_first_name": contactForm.first_name.value,
         "from_last_name": contactForm.last_name.value,
-        "from_email": contactForm.emailaddress.value,
-        "interested_in": contactForm.interested.value,
-        "message_request": contactForm.messagesummary.value
+        "from_email": contactForm.email.value,
+        "interested_in": contactForm.interested_in.value,
+        "message": contactForm.message.value,
     })
     .then(
         function(response) {
