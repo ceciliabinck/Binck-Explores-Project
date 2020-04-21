d5e49f3754f1f74a15211e7708263a4b1dd68baf
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-21 02:13:46 +0200

    Trying different options for gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,8 @@
+var caption = document.querySelector("#gallery figcaption"),
+    thumbs = document.getElementById("thumbs"),
+    fullsize = document.getElementById("fullsize");
+thumbs.addEventListener("click", function(e) {
+    var filename = e.target.src.split("/").pop();
+    fullsize.src = "assets/img/Gallery/full" + filename;
+    caption.innerHTML = e.target.alt;
+})
