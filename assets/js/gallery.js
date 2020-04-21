de34b48fa5cf34ac57c2b197f55877fea86bafcd
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-21 20:57:07 +0200

    Trying to put my gallery together

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,12 +1,33 @@
-var caption = document.querySelector("#gallery figcaption"),
+/*var caption = document.querySelector("#gallery figcaption"),
     thumbs = document.getElementById("thumbs"),
     fullsize = document.getElementById("fullsize");
 thumbs.addEventListener("click", function(e) {
     var filename = e.target.src.split("/").pop();
     fullsize.src = "assets/img/Gallery/full" + filename;
     caption.innerHTML = e.target.alt;
-})
+})*/
 
-function popupImg(params) {
 
-}
+
+$("input").click(function() {
+    console.log("Hello")
+});
+
+function getpopupImg() {
+    fetch(`assets/json/gallery.json?image=${image-src}&description=${description}&type=text`)
+    if (response) {
+
+    }(response => response.json())
+    .then(rawData => {
+        generateQuestionsAnswers(rawData);
+    });
+    console.log("${description}");
+
+};
+
+/*let description = gallery.json
+for (let i = 0; i < description.length; i++) {
+    console.log(description[i] + "Hello");
+let img = $(this).attr("src")
+        let newimg = "image-src"
+}*/
