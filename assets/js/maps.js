da7f91beae3f9b61ab22bd393461570fbf1bfa3f
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-21 20:57:55 +0200

    Commented it out will change this

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-function initMap() {
+/*function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
@@ -8,23 +8,23 @@ function initMap() {
     });
 
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lacations = [
-        /* Canada */
-        { lat: 59.913869, lng: 10.752245 }, /* lake louise */
-        { lat: 51.178363, lng: -115.570769 }, /* Banff */
-        { lat: 53.544389, lng: -113.490927 }, /* Edmonton */
-        { lat: 51.048615, lng: -114.070846 }, /* Calgary */
-        { lat: 49.285332, lng: -123.129916 } /* Vancover */
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lacations = [
+    /* Canada 
+    { lat: 59.913869, lng: 10.752245 }, /* lake louise 
+    { lat: 51.178363, lng: -115.570769 }, /* Banff 
+    { lat: 53.544389, lng: -113.490927 }, /* Edmonton
+    { lat: 51.048615, lng: -114.070846 }, /* Calgary 
+    { lat: 49.285332, lng: -123.129916 } /* Vancover 
 
-    ];
-    var markers = locations.map(function(location, i){
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.lenght]
-        });
+];
+var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.lenght]
     });
+});
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
-}         
+}*/
