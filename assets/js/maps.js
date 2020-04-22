44aa4f651f30224f8434541c1559343299c5fe3a
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-22 18:26:46 +0200

    Testing leaflet map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,30 +1,10 @@
-/*function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
-    });
+var mymap = L.map('#mapid').setView([51.505, -0.09], 13);
 
-
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lacations = [
-    /* Canada 
-    { lat: 59.913869, lng: 10.752245 }, /* lake louise 
-    { lat: 51.178363, lng: -115.570769 }, /* Banff 
-    { lat: 53.544389, lng: -113.490927 }, /* Edmonton
-    { lat: 51.048615, lng: -114.070846 }, /* Calgary 
-    { lat: 49.285332, lng: -123.129916 } /* Vancover 
-
-];
-var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.lenght]
-    });
-});
-
-var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-
-}*/
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
