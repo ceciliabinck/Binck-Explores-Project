7ceaa3c64f898c0b13c74cd23c8d21b9426ad309
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-23 00:17:24 +0200

    Created function that appends set data in modal

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,48 +1,26 @@
-/*var caption = document.querySelector("#gallery figcaption"),
-    thumbs = document.getElementById("thumbs"),
-    fullsize = document.getElementById("fullsize");
-thumbs.addEventListener("click", function(e) {
-    var filename = e.target.src.split("/").pop();
-    fullsize.src = "assets/img/Gallery/full" + filename;
-    caption.innerHTML = e.target.alt;
-})*/
-
 /** 
  * Load function for retrieving local JSON file.
  */
 window.onload = function() {
-        fetch(`assets/json/gallery.json`)
-            .then(response => response.json())
-            .then(data => {
+    fetch(`assets/json/gallery.json`)
+        .then(response => response.json())
+        .then(data => {
+            loadData(data)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
 
-                loadData(data)
-
-            })
-            .catch(error => {
-
-                    console.log(error)
-
-                }
-
-            );
-
-    }
-    /** 
-     * get data and append in modal
-     */
-
+/** 
+ * Get the loadData of set index to append in modal after clicking modal button.
+ */
 function loadData(dataSource) {
     let imageDetails = dataSource;
-    // console.log(imageDetails)
-    console.log(imageDetails[1].description)
 
-
-    $("img").click(function() {
-        $("#descript").append(`<p id="descript">${imageDetails[1].description}</p>`);
+    $("#pic-pop").click(function() {
+        $("#modal-img").append(`<img id="model-img" class="img-fluid" src="${imageDetails[3].imagesrc}"/>`);
+        $("#descript").append(`<p id="descript">${imageDetails[3].description}</p>`);
         console.log("modal click test ")
-
-
-
-
     })
 }
