76e2b3756a2c2ed2f4f200932366d21aaf47d290
Cecilia Binck <cecilia4binck@gmail.com>
2020-04-27 22:32:44 +0200

    Changes structure of comments

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,11 +1,23 @@
 $("img").click(function() {
-    /**Makes sure the id is empty before beginning */
+
+    /**
+     * Makes sure the id is empty before beginning
+     */
+
     $('#modal-img').empty();
     $("#descript").empty();
-    /**The path to get the data */
+
+    /**
+     * The path to get the data
+     */
+
     let imageSource = $(this).attr("src");
     let imageDescription = $(this).attr("alt")
-        /**putting the data in the modal */
+
+    /**
+     * putting the data in the modal
+     */
+
     $("#modal-img").append(`<img class="img-fluid" src="${imageSource}"/>`);
     $("#descript").append(`<p>${imageDescription}</p>`);
 })
