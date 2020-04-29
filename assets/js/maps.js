98d78924e91089385dc5833a6689e85553e07412
Cecilia <cecilia4binck@gmail.com>
2020-04-29 20:46:36 +0200

    Put in beginning map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,7 @@
-var mymap = L.map('#mapid').setView([51.505, -0.09], 13);
+var map = l.map("mapid").setView([0, 0], 1);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+L.tilelayer("https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=XDLrWwttX86zuGfbH7sI", {
+attribution: "<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>"
+}).addTo(mapid);
+
+var marker = L.marker([51.5, -0.09]).addTo(mapid);
