fe655f6df0a01d9e6482ceb03ec9f7772fdd61e3
Cecilia <cecilia4binck@gmail.com>
2020-05-04 20:00:01 +0200

    Changed some mistakes

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
-var map = l.map("mapid").setView([0, 0], 1);
+var map = L.map("mapid").setView([0, 0], 1);
 
 L.tilelayer("https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=XDLrWwttX86zuGfbH7sI", {
-attribution: "<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>"
-}).addTo(mapid);
+attribution: <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>
+})
+.addTo(mapid);
 
-var marker = L.marker([51.5, -0.09]).addTo(mapid);
