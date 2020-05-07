02531537b9ec356001ad61558a183ccd61b148d1
Cecilia <cecilia4binck@gmail.com>
2020-05-07 20:03:26 +0200

    Added the setview of map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,10 @@
-var map = L.map("mapid").setView([0, 0], 1);
+mymap = L.map('mapid').setView([25, 15], 2);
 
-L.tilelayer("https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=XDLrWwttX86zuGfbH7sI", {
-attribution: <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>
-})
-.addTo(mapid);
-
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 13,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiY2VjaWxpYS04NiIsImEiOiJjazl3eDdoZ3cwNXViM2dsaXR4NWF2cmVyIn0.0NitNMOdk7g1iy_6M1Ce2g'
+}).addTo(mymap);
