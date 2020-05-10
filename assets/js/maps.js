c872d9dfab8a103870da28746751c0a6ea823d0c
ceciliabinck <cecilia4binck@gmail.com>
2020-05-10 08:38:25 +0000

    Tried to put in a modal to show off images

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -81,5 +81,27 @@ searchControl.on("results", function (data) {
 });
 
 
+$("bindPopup").click(function() {
 
+    /**
+     * Makes sure the id is empty before beginning
+     */
+
+    $('#modal-img').empty();
+    $("#descript").empty();
+
+    /**
+     * The path to get the data
+     */
+
+    let imageSource = $(this).attr("src");
+    let imageDescription = $(this).attr("alt")
+
+    /**
+     * putting the data in the modal
+     */
+
+    $("#modal-img").append(`<img class="img-fluid" src="${imageSource}"/>`);
+    $("#descript").append(`<p>${imageDescription}</p>`);
+})
 
